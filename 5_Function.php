2f561d872d3d2096bafaa9914a2d6ffb64bcb83b
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-01-07 02:56:27 +0000

    Create 5_Function.php

---
+++ 5_Function.php
@@ -0,0 +1,11 @@
+<!-- What is a Function in PHP?
+You must have come across the word function. Let’s know the meaning of it. A Function is a reusable piece or block of code that performs some action. It takes input from the user in the form of parameters, performs certain actions, and gives the output. Functions can either return values when called or can simply perform an operation without returning any value.
+ -->
+function print_number($number){
+        echo "<br>Your number is ";
+        echo $number;
+    }
+
+<!-- 
+NB if we want to use global variables in a local scope then we simply use gklobal keyword 
+-->
