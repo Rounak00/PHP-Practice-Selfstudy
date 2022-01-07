9153132d250d186c4a1b9b58d29af321023b60bb
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-01-07 02:33:10 +0000

    Create 4_LOOPs.php

---
+++ 4_LOOPs.php
@@ -0,0 +1,25 @@
+<!-- Loops
+Loops are used to execute the same block of code until a certain condition is met. Loop is to automate the repetitive tasks within a program to save time and effort. PHP supports four different types of loops.
+
+While:-  the block of code is executed until the condition is true.
+do…while:- the block of code executed once and then the condition is evaluated. If the condition is true, the statement is repeated as long as the condition is true.
+For:- the block of code is executed until the condition is true.
+Foreach:- loops through a block of code for each element in an array. -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Tutorials</title>
+</head>
+<body>
+    <div class="container">
+      <?php
+            $lan=array(10,20,3,045,87,9);
+         
+      ?>
+    </div>
+</body>
+</html>
