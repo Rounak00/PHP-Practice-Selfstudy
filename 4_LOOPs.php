33d015b2ac236e7a3f86ce73e1c82b9889abda84
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-01-07 02:36:20 +0000

    Update 4_LOOPs.php

--- 4_LOOPs.php
+++ 4_LOOPs.php
@@ -17,8 +17,42 @@ Foreach:- loops through a block of code for each element in an array. -->
 <body>
     <div class="container">
       <?php
-            $lan=array(10,20,3,045,87,9);
-         
+            $languages=array(10,20,3,045,87,9);
+       $a = 0;
+ 
+     
+    while ($a <= 10) {
+        echo "<br>The value of a is: ";
+        echo $a;
+        $a++;
+    }
+
+    // Iterating arrays in PHP using while loop
+    $a = 0;
+    while ($a < count($languages)) {
+        echo "<br>The value of language is: ";
+        echo $languages[$a];
+        $a++;
+    }
+
+    // Do while loop
+    $a = 200;
+    do {
+        echo "<br>The value of a is: ";
+        echo $a;
+        $a++;
+    } while ($a < 10);
+
+    // For loop
+    for ($a=60; $a < 10; $a++) { 
+        echo "<br>The value of a from the for loop is: ";
+        echo $a;
+    }
+
+    foreach ($languages as $value) {
+        echo "<br>The value from foreach loop is ";
+        echo $value;
+    }
       ?>
     </div>
 </body>
