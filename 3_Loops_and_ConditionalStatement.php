5310094e3156ae70682d3bff499f7ae5b6acb9ed
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-01-07 02:27:38 +0000

    Create 3_Loops_and_ConditionalStatement.php

---
+++ 3_Loops_and_ConditionalStatement.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Tutorials</title>
+</head>
+<body>
+    <div class="container">
+      <?php
+             $age = 6;
+    if($age>18){
+        echo "You can go to the party";
+    }
+    else if($age==7){
+        echo "You are 7 years old";
+    }
+    else if($age==6){
+        echo "You are 6 years old";
+    }
+    else{
+        echo "You can not go to the party";
+    }
+      ?>
+    </div>
+</body>
+</html>
+
+<!--  Switch
+
+<?php
+$favcolor = "red";
+
+switch ($favcolor) {
+  case "red":
+    echo "Your favorite color is red!";
+    break;
+  case "blue":
+    echo "Your favorite color is blue!";
+    break;
+  case "green":
+    echo "Your favorite color is green!";
+    break;
+  default:
+    echo "Your favorite color is neither red, blue, nor green!";
+}
+?>  -->
+
